8c477c024e9c0b4db0d3d8f6935e5f2d332da2be
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-04 11:52:41 +0900

    Add files via upload

---
+++ sway-setup-freebsd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo pkg install wayland xwayland seatd \
+  fuzzel foot alacritty nwg-drawer nwg-launchers \
+  waybar swaybg swaylock waylock swayidle \
+  wf-recorder wl-clipboard wob wlogout obs-studio \
+  noto-jp fcitx5-im fcitx5-configtool ja-fcitx5-anthy \
+  xbrightness pipewire pavucontrol wireplumber \
+  firefox \
+  gvfs fusefs-ntfs xdg-user-dirs \
+  xarchiver nemo nemo-fileroller mako volumeicon kanshi \
+  sway
